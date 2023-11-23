8384401e06b1be6dcefac468e79790b487c8a517
safar.radim <xsafar27@stud.fit.vutbr.cz>
2023-11-23 15:50:07 +0100

    feat: added config file

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask
 from models import *
+from yaml import load, FullLoader
 
+with open("config.yaml") as f:
+    cfg = load(f, Loader=FullLoader)
+
+username = cfg["user"]["name"]
+password = cfg["user"]["password"]
+
+ip = cfg["server"]["IP"]
+port = cfg["server"]["port"]
+
+database = cfg["database"]["name"]
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:changeme@localhost/database"
+app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{username}:{password}@{ip}:{port}/{database}'
 db.init_app(app)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello world!</p>"
+
+@app.route("/users")
+def users():
+    user_list = User.get_list()
+    print(user_list)
+    return str(user_list)
+    
