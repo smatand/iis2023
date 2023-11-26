b30f1b3a126f5d4dbfe08bbcf8c051528bca2d87
smatand <andrejsmatana0@gmail.com>
2023-11-26 16:47:57 +0100

    add edit_event button when viewing the details

--- forms.py
+++ forms.py
@@ -57,6 +57,41 @@ class EventForm(FlaskForm):
             ]
 
 
+class EditEventForm(FlaskForm):
+    start_datetime = DateTimeField(
+        'Start Date and Time',
+        format='%Y-%m-%dT%H:%M',
+        validators=[DataRequired()],
+        default=datetime.now() + timedelta(minutes=30)
+    )
+    end_datetime = DateTimeField(
+        'End Date and Time',
+        format='%Y-%m-%dT%H:%M',
+        validators=[DataRequired(), validate_date],
+        default=datetime.now() + timedelta(hours=1)
+    )
+    capacity = IntegerField('Capacity', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    image = StringField('Image URL')
+    place_id = SelectField('Place', validators=[DataRequired()], coerce=int)
+    category_ids = SelectMultipleField('Category',
+                                       validators=[Optional()],
+                                       coerce=int,
+                                       option_widget=widgets.CheckboxInput(),
+                                       widget=widgets.ListWidget(
+                                           prefix_label=False
+                                           ))
+
+    submit = SubmitField('Submit changes')
+
+    def __init__(self, *args, **kwargs):
+        super(EditEventForm, self).__init__(*args, **kwargs)
+        self.place_id.choices = [(p.id, p.name) for p in Place.query.all()]
+        self.category_ids.choices = [
+            (c.id, c.name) for c in Category.query.all()
+            ]
+
+
 class PlaceForm(FlaskForm):
     name = StringField('Place Name', validators=[DataRequired()])
     address = StringField('Address', validators=[DataRequired()])
