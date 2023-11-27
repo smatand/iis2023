f772667946acf3248da5828659a284624df2b13d
smatand <andrejsmatana0@gmail.com>
2023-11-27 16:10:18 +0100

    set timeout of session to 30mins

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def load_user(user_id):
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=10)
+    app.permanent_session_lifetime = timedelta(minutes=30)
     session.modified = True
 
 
